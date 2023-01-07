dbb795aa022cc451aacf7c0de27c541405aa013a
Ansh Bansal <anshbansal19906@gmail.com>
2023-01-08 02:20:29 +0530

    created the full funtional responsive site.

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 const botn = document.getElementById("addbtn");
+const stg = document.getElementById("stage");
+
+function camelCase(str) {
+    let arr = str.split(" ");
+
+
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+    }
+
+    return arr.join(" ");
+}
 
 function Book(title, author, pageno, status) {
     this.title = title;
@@ -16,29 +28,50 @@ function Book(title, author, pageno, status) {
 }
 
 let myLibrary = [];
+let i = 0;
 
 function addBookToLibrary() {
     let name = prompt("Enter book's title:");
     let author = prompt("Enter author's name:");
     let pageno = prompt("Enter number of pages in the book:");
-    let status = prompt("Have you read the book or not?");
+    let status = window.confirm("Can this book be considered as read?")
     let book = new Book(name, author, pageno, status);
     myLibrary.push(book);
     displayLib();
 }
 
 function displayLib() {
-    for(let i=0; i<myLibrary.length; i++){
-        const crd = document.createElement('div');
-        const titl = document.createElement('div');
-        const inf = document.createElement('div');
-        const stat = document.createElement('div');
+    //for(let i=0; i<myLibrary.length; i++){
+        var crd = document.createElement('div');
+        var titl = document.createElement('div');
+        var inf = document.createElement('div');
+        var stat = document.createElement('button');
         titl.classList.add("title");
         inf.classList.add("info");
         stat.classList.add("status");
         crd.classList.add("card");
-        
-    }
+        titl.textContent = camelCase(myLibrary[i].title);
+        inf.textContent = camelCase(myLibrary[i].title) +" by "+camelCase(myLibrary[i].author)+", "+myLibrary[i].pageno+" pages.";
+        if(myLibrary[i].status == true){
+            stat.textContent="READ";
+        }
+        else{
+            stat.textContent="NOT READ";
+        }
+        crd.appendChild(titl);
+        crd.appendChild(inf);
+        crd.appendChild(stat);
+        stg.appendChild(crd);
+        stat.addEventListener("click", function(){
+            if(stat.textContent == "NOT READ"){
+                stat.textContent="READ";
+            }
+            else{
+                stat.textContent="NOT READ";
+            }
+        });
+        i++;
+    //}
 }
 
 botn.addEventListener("click", addBookToLibrary);
